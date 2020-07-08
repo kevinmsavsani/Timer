b8db2a6d9d3e2e717128ef5fc4250742211be6aa
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-08 16:12:29 +0530

    redux connected

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './components/Timer/reducers';
 import Timer from './components/Timer';
+
+const store = createStore(reducer);
 
 export default class App extends React.Component {
 	render() {
 		return (
+			<Provider store={store}>
 				<Timer />
+			</Provider>
 		);
 	}
 }
--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators as actions } from './actions';
 
 import Button from '../Button';
 
-export default class Timer extends React.Component {
+function formatTime(time) {
+	let minutes = Math.floor(time / 60);
+	time -= minutes * 60;
+	let seconds = parseInt(time % 60, 10);
+	return `${minutes < 10 ? `0${minutes}` : minutes}:${
+		seconds < 10 ? `0${seconds}` : seconds
+	}`;
+}
+
+class Timer extends React.Component {
+	componentWillReceiveProps(nextProps) {
+		const currentProps = this.props;
+		if (!currentProps.isPlaying && nextProps.isPlaying) {
+			// start the interval
+			const timerInterval = setInterval(() => {
+				currentProps.addSecond();
+			}, 1000);
+			this.setState({ timerInterval });
+		} else if (currentProps.isPlaying && !nextProps.isPlaying) {
+			// stop the interval
+			clearInterval(this.state.timerInterval);
+		}
+	}
 	render() {
+		const {
+			isPlaying,
+			elapsedTime,
+			timerDuration,
+			startTimer,
+			restartTimer
+		} = this.props;
 
 		return (
 			<View style={styles.container}>
 				<StatusBar barStyle={'light-content'} />
 				<View style={styles.upper}>
 					<Text style={styles.time}>
-						25:00
+						{formatTime(timerDuration - elapsedTime)}
 					</Text>
 				</View>
 				<View style={styles.lower}>
-						<Button iconName={'play-circle'} onPress={() => Alert.alert('Start Timer!')} />
-						<Button iconName={'stop-circle'} onPress={() => Alert.alert('Start Timer!')} />
+					{!isPlaying && (
+						<Button iconName={'play-circle'} onPress={startTimer} />
+					)}
+					{isPlaying && (
+						<Button iconName={'stop-circle'} onPress={restartTimer} />
+					)}
 				</View>
 			</View>
 		);
@@ -44,3 +80,25 @@ const styles = StyleSheet.create({
 		fontWeight: '100'
 	}
 });
+
+function mapStateToProps(state) {
+	const { isPlaying, elapsedTime, timerDuration } = state;
+	return {
+		isPlaying,
+		elapsedTime,
+		timerDuration
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		startTimer: bindActionCreators(actions.startTimer, dispatch),
+		restartTimer: bindActionCreators(actions.restartTimer, dispatch),
+		addSecond: bindActionCreators(actions.addSecond, dispatch)
+	};
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Timer);
