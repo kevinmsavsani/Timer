be04d9bb5d32cfa2bdb86bc1b25779651ef20513
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-08 15:46:46 +0530

    Timer view

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Alert } from 'react-native';
 
 export default class Timer extends React.Component {
-	render () {
+	render() {
+
 		return (
-		<View style={styles.container}>
-		  <Text>Timer Component</Text>
-		</View>
-	  );
+			<View style={styles.container}>
+				<StatusBar barStyle={'light-content'} />
+				<View style={styles.upper}>
+					<Text style={styles.time}>
+						25:00
+					</Text>
+				</View>
+				<View style={styles.lower}>
+					<Text style={{color:'#ffffff'}}> Start and stop button </Text>
+				</View>
+			</View>
+		);
 	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#000000'
+	},
+	upper: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	lower: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	time: {
+		color: '#ffffff',
+		fontSize: 120,
+		fontWeight: '100'
+	}
 });
